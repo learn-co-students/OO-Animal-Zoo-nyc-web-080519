72c0ca6a6809e5527e00cbfe731f83ebed0a1d75
kgriswo1 <kgriswo1@binghamton.edu>
2019-08-10 13:15:46 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    attr_accessor :weight, :zoo
+    attr_reader :specie, :nickname
+    @@all = []
 
+    def initialize(specie, weight, nickname, zoo)
+        @specie = specie
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    #Take an Animal species as argument
+    #Return an Array of all the animals that are that species
+    def self.find_by_species(given_specie)
+        self.all.select {|animal| animal.specie == given_specie}
+    end
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,20 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+zoo1 = Zoo.new("The Happy Zoo", "New York")
+zoo2 = Zoo.new("The Sad Zoo", "New Jersey")
+zoo3 = Zoo.new("The Angry Zoo", "Binghamton")
+zoo4 = Zoo.new("The Lonely Zoo", "Hartsdale")
+
+animal1 = Animal.new("Cat", 10, "GuaGua", zoo1)
+animal2 = Animal.new("Dog", 15, "Oliver", zoo1)
+animal3 = Animal.new("Cat", 18, "Bill", zoo2)
+animal4 = Animal.new("Cat", 40, "Stacy", zoo1)
+animal5 = Animal.new("Dog", 10, "Jake", zoo2)
+
+# Animal.find_by_species("Cat")
+# zoo1.animals
+# zoo1.find_by_species("Cat")
 
 
 binding.pry
