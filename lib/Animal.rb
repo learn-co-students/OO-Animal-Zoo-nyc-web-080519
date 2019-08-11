108eb5a280f44e29de3edf678e210a55f58a4340
DanBehrman <dan.behrman@wework.com>
2019-08-11 13:09:49 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight
 
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+    end
+
+    def zoo
+        Zoo.all.select do |zoo|
+            zoo.animals.include?(self)
+        end[0]
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        all.select do |animal|
+            animal.species == species
+        end
+    end
 end
