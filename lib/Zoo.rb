05ba9f2f69460d1c443799200d232c30fad5935e
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-11 18:48:32 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,64 @@
 class Zoo
 
-end
+  attr_accessor :name, :location
+
+  @@all = []
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # Zoo.find_by_location should take in a location as an argument and return an array of all the zoos within that location.
+  def self.find_by_location(location)
+    self.all.select {|zoo| zoo.location == location }
+  end
+
+  def add_animal(species, weight, nickname)
+    Animal.new(species, weight, nickname, self)
+  end
+
+  # Zoo#animals should return all the animals that a specific instance of a zoo has.
+  def animals
+    my_animals = []
+    Animal.all.each do |animal|
+      my_animals << animal if animal.zoo == self 
+    end
+    my_animals
+  end
+
+  # Zoo#animal_species should return an array of all the species (as strings) of the animals in the zoo. However, if you have two dogs, it should only return one "Dog" string (aka an unique array).
+  def animal_species
+    species = []
+    animals.each do |animal|
+      species << animal.species
+    end
+    species.uniq
+  end
+
+  # Zoo#find_by_species should take in an animal's species as an argument and return an array of all the animals in that zoo, which are of that species.
+  def find_by_species(species)
+    animals.select do |animal|
+      animal.species == species
+    end
+  end
+
+  # Zoo#animal_nicknames should return an array of all the nicknames of animals that a specific instance of a zoo has.
+  def animal_nicknames
+    nicknames = []
+    animals.each do |animal|
+      nicknames << animal.nickname
+    end
+    nicknames
+  end
+
+  
+
+
+end #end Zoo class
+
--- run.rb
+++ run.rb
@@ -5,6 +5,25 @@ require 'pry'
 
 #Test your code here
 
+z1 = Zoo.new("Zoo1", "Bronx")
+z2 = Zoo.new("Zoo2", "Washington")
+z3 = Zoo.new("Zoo3", "Cleveland")
+z4 = Zoo.new("Zoo4", "Central Park")
+
+a1 = z1.add_animal("elephant", 2000, "Dumbo")
+a2 = z1.add_animal("monkey", 35, "chimpy")
+a3 = z1.add_animal("giraffe", 500, "Tall Boy")
+a4 = Animal.new("lion", 600, "Rory", z2)
+a5 = Animal.new("cheetah", 600, "Speedy", z2)
+a6 = Animal.new("turtle", 600, "Shelly", z3)
+a7 = Animal.new("panda", 600, "Spot", z4)
+a8 = z2.add_animal("elephant", 2050, "Stompy")
+a9 = z1.add_animal("monkey", 50, "Marcel")
+a10 = z1.add_animal("lion", 500, "Simba")
+
+
+
+
 
 binding.pry
 puts "done"
